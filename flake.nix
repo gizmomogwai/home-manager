56f171d11417a8cfa03938db512f98e87be0f7fb
Christian Koestlin <christian.koestlin@accenture.com>
2025-10-27 08:35:50 +0100

    Prepare initial home-manager setup in ~/.config/home-manager based on https://github.com/evertras/simple-homemanager

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "My home-manager flake";
+  inputs = {
+    nixpkgs = {
+      url = "nixpkgs/nixos-25.05";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system;  config.allowUnfree = true; };
+    in {
+      homeConfigurations = {
+        christian-koestlin = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
+    };
+}
