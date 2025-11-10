7a656c5074c0b90bbb6bb8e85ea52dce686c058c
Christian Koestlin <christian.koestlin@gmail.com>
2025-11-10 18:48:19 +0100

    Remove zenbrowser

--- common-packages.nix
+++ common-packages.nix
@@ -1,25 +1,34 @@
 { pkgs }:
 with pkgs; [
-      vlc
-      enlightenment.ephoto
-      vscode
-      cargo
-      rust-analyzer
-      rustc
-      git
-      tig
-      jq
-      httpie
-      htop
-      joe
-      emacs
-      ripgrep
       babelfish
-      lolcat
-      spotify
+      cargo
+      clippy
       dive
-      podman
-      podman-desktop
+      emacs
+      enlightenment.ephoto
       fd
       firefox
+      fish
+      git
+      htop
+      httpie
+      joe
+      jq
+      jujutsu
+      lolcat
+      podman
+      podman-desktop
+      quodlibet
+      ripgrep
+      rust-analyzer
+      rustc
+      rustfmt
+      spotify
+      taglib
+      tig
+      tree
+      vlc
+      vscode
+      wine
 ]
+
--- flake.nix
+++ flake.nix
@@ -8,12 +8,13 @@
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
   outputs = { nixpkgs, home-manager, ... }:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system;  config.allowUnfree = true; };
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
     in {
       homeConfigurations = {
         christian-koestlin = home-manager.lib.homeManagerConfiguration {
