3c79621d729fa57ad5c5228e71ab2ed1c7bdf462
Christian Köstlin <christian.koestlin@gmail.com>
2026-01-01 16:57:13 +0100

    Update nixpkgs

--- common-packages.nix
+++ common-packages.nix
@@ -4,21 +4,27 @@ with pkgs; [
       dive
       emacs
       enlightenment.ephoto
+      evince
       fd
       firefox
       fish
       git
       htop
       httpie
+      icloudpd
       joe
       jq
       jujutsu
       lazyjj
       lolcat
+#      mpd
       podman
       podman-desktop
+#      qmmp
       quodlibet
+      rclone
       ripgrep
+      rmpc
       rust-bin.stable."1.91.1".default
       spotify
       taglib
@@ -27,5 +33,7 @@ with pkgs; [
       vlc
       vscode
       wine
+      yazi
+      zeal
 ]
 
