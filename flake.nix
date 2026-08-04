8e026452ffb43adde3cbf3381e81016d99585d4d
Christian Köstlin <christian.koestlin@gmail.com>
2026-08-04 16:55:28 +0200

    Get rid of the nixgl workaround (not needed for Intel, lets see on my other machine)
    Change-Id: Ia12c49d9fe3fb9e746e29200556685258c2bedcf

--- flake.nix
+++ flake.nix
@@ -14,49 +14,12 @@
   outputs = { nixpkgs, home-manager, rust-overlay, nixgl, ... }:
     let
       lib = nixpkgs.lib;
-      system = "x86_64-linux";
-      nixglOverlay = final: _:
-        let
-          nixglPatched = final.applyPatches {
-            name = "nixGL-patched";
-            src = nixgl.outPath;
-            patches = [
-              (final.writeText "nixgl-nvidia-libs-only.patch" ''
-                diff --git a/nixGL.nix b/nixGL.nix
-                --- a/nixGL.nix
-                +++ b/nixGL.nix
-                @@ -86,8 +86,7 @@ let
-                         });
-
-                       nvidiaLibsOnly = nvidiaDrivers.override {
-                         libsOnly = true;
-                -        kernel = null;
-                       };
-
-                       nixGLNvidiaBumblebee = writeExecutable {
-              '')
-            ];
-            postPatch = ''
-              substituteInPlace nixGL.nix \
-                --replace-fail \
-                  'data = builtins.readFile _nvidiaVersionFile;' \
-                  'data = builtins.readFile _nvidiaVersionFile;
-                  versionLine = builtins.head (lib.splitString "\n" data);' \
-                --replace-fail \
-                  'versionMatch = builtins.match ".*Module  ([0-9.]+)  .*" data;' \
-                  'versionMatch = builtins.match ".*  ([0-9][0-9.]+)  .*" versionLine;'
-            '';
-          };
-          isIntelX86Platform = final.stdenv.hostPlatform.system == "x86_64-linux";
-        in {
-          nixgl = import "${nixglPatched}/default.nix" {
-            pkgs = final;
-            enable32bits = isIntelX86Platform;
-            enableIntelX86Extensions = isIntelX86Platform;
-          };
-        };
-      overlays = [ (import rust-overlay) nixglOverlay ];
-      pkgs = import nixpkgs { inherit overlays system; config.allowUnfree = true; };
+      overlays = [ (import rust-overlay) nixgl.overlay ];
+      pkgs = import nixpkgs {
+        inherit overlays;
+        localSystem = "x86_64-linux";
+        config.allowUnfree = true;
+      };
       username = builtins.getEnv "USER";
       hostName = builtins.getEnv "HOSTNAME";
       hostModule = ./. + "/${hostName}.nix";
